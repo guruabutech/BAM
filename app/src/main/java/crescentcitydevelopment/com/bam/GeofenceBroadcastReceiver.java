7a1f2f23f3e39a7a19b85d3609e94bbfa169c8cf
Redando <Redandof@gmail.com>
2017-07-17 16:49:03 -0500

    Geofence & Broadcast Receiver

---
+++ app/src/main/java/crescentcitydevelopment/com/bam/GeofenceBroadcastReceiver.java
@@ -0,0 +1,13 @@
+package crescentcitydevelopment.com.bam;
+
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class GeofenceBroadcastReceiver extends BroadcastReceiver{
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
---
+++ app/src/main/java/crescentcitydevelopment/com/bam/Geofencing.java
@@ -0,0 +1,109 @@
+package crescentcitydevelopment.com.bam;
+
+
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.Result;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.ResultCallbacks;
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.GeofencingRequest;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.PlaceBuffer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Geofencing implements ResultCallback {
+    private List<Geofence> mGeofenceList;
+    private GoogleApiClient mGoogleApiClient;
+    private PendingIntent mGeofencePendingIntent;
+    private Context mContext;
+    private static final float GEOFENCE_RADIUS = 50; // 50 meters
+    private static final long GEOFENCE_TIMEOUT = 24 * 60 * 60 * 1000; // 24 hours
+
+    public Geofencing(Context context, GoogleApiClient client){
+        mContext = context;
+        mGoogleApiClient = client;
+        mGoogleApiClient = null;
+        mGeofenceList = new ArrayList<>();
+    }
+    public void registerAllGeofences(){
+        if(mGoogleApiClient == null || !mGoogleApiClient.isConnected() ||
+                mGeofenceList == null || mGeofenceList.size() == 0){
+            return;
+        }
+        try {
+            LocationServices.GeofencingApi.addGeofences(
+                    mGoogleApiClient,
+                    getGeofencingRequest(),
+                    getGeofencePendingIntent()
+                    ).setResultCallback(this);
+        }catch (SecurityException securityException){
+
+        }
+    }
+    public void unregisterAllGeofences(){
+        if(mGoogleApiClient == null || !mGoogleApiClient.isConnected()){
+            return;
+        }
+        try {
+            LocationServices.GeofencingApi.removeGeofences(
+                    mGoogleApiClient,
+                    getGeofencingRequest()
+            ).setResultCallback(this);
+        }catch (SecurityException securityException){
+
+        }
+    }
+
+
+
+    public void updateGeofencesList(PlaceBuffer places){
+        mGeofenceList = new ArrayList<>();
+        if(places == null || places.getCount() == 0) return;
+        for(Place place : places){
+            String placeUID = place.getId();
+            double placeLat = place.getLatLng().latitude;
+            double placeLng = place.getLatLng().longitude;
+
+            Geofence geofence = new Geofence.Builder()
+                    .setRequestId(placeUID)
+                    .setExpirationDuration(GEOFENCE_TIMEOUT)
+                    .setCircularRegion(placeLat, placeLng, GEOFENCE_RADIUS)
+                    .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER | Geofence.GEOFENCE_TRANSITION_EXIT)
+                    .build();
+            mGeofenceList.add(geofence);
+        }
+    }
+
+    private GeofencingRequest getGeofencingRequest(){
+        GeofencingRequest.Builder builder = new GeofencingRequest.Builder();
+        //If user is already inside, trigger entry event
+        builder.setInitialTrigger(GeofencingRequest.INITIAL_TRIGGER_ENTER);
+        //Add geofence to ArrayList & Build it
+        builder.addGeofences(mGeofenceList);
+        return builder.build();
+    }
+
+    private PendingIntent getGeofencePendingIntent(){
+        if(mGeofencePendingIntent != null){
+            return mGeofencePendingIntent;
+        }
+        Intent intent = new Intent(mContext, GeofenceBroadcastReceiver.class);
+        mGeofencePendingIntent = PendingIntent.getBroadcast(mContext, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        return mGeofencePendingIntent;
+    }
+
+    @Override
+    public void onResult(@NonNull Result result) {
+
+    }
+}
